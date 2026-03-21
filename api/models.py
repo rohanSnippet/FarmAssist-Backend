ea695bff08b276631f949e4d77e7067841bf4291
Rohan M <rohan110620@gmail.com>
2026-03-21 20:44:07 +0530

    added the gemini for soil health cards parameter recognition

---
+++ api/models.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        
+        if not extra_fields.get('first_name'):
+            raise ValueError("First name is required")
+        
+        if 'auth_providers' not in extra_fields:
+            extra_fields['auth_providers'] = ['email']
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('auth_providers', ['email'])
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    username = None  # Remove the username field
+    email = models.EmailField(unique=True)  # Make email the unique identifier
+    first_name = models.CharField(max_length=150, blank=False)  # required
+    last_name = models.CharField(max_length=150, blank=True)    # optional
+    photo_url = models.URLField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, unique=True, blank=True, null=True)
+    auth_providers = models.JSONField(default=list, blank=True)
+    location_label = models.CharField(max_length=255, blank=True, null=True, help_text="e.g. Mumbai, India")
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    
+    USERNAME_FIELD = 'email'  # Tell Django to use email for login
+    REQUIRED_FIELDS = ['first_name']      # Email is required by default, so leave this empty
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
---
+++ api/serializers.py
@@ -0,0 +1,53 @@
+# api/serializers.py
+from rest_framework import serializers
+from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id","first_name", "last_name", "email", "password", "photo_url", "phone_number", "auth_providers", "location_label", "latitude", "longitude"]
+        extra_kwargs = {"password": {"write_only": True}}
+        read_only_feilds = ["email", "auth_providers"]
+    
+    def validate_email(self, value):
+        if User.objects.filter(email= value).exists():
+            raise serializers.ValidationError("User already exists")
+        return value
+    
+    def validate_phone_number(self, value):
+        if value == "":
+            return None
+        return value
+    
+    def create(self, validated_data):
+        # We use create_user to ensure password hashing happens
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+
+        token = super().get_token(user)
+
+        # 2. Add custom claims
+        token['email'] = user.email
+        # You can add more fields here if you want:
+        # token['username'] = user.username
+        # token['is_admin'] = user.is_superuser
+
+        return token
+    
+class UpdateProfileSerializer(serializers.Serializer):
+    """
+    Used when a user (e.g., logged in via Phone) wants to add an Email/Google account.
+    """
+    email = serializers.EmailField()
+    provider = serializers.CharField(required=False) # e.g., 'google'
+
+    def validate_email(self, value):
+        # REQUIREMENT 5: Check if email exists in another account before allowing update
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already associated with another account.")
+        return value
