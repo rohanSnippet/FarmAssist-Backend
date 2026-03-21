8a0b646d9624b416d2037feb4cad2ec34931a8d1
Rohan M <rohan110620@gmail.com>
2026-03-21 20:44:37 +0530

    added the gemini for soil health cards parameter recognition

---
+++ backend/urls.py
@@ -0,0 +1,22 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from api.views import CreateUserView, UserDetailView, CustomTokenObtainPairView, FirebaseAuthView, LinkAccountView
+from recommendation.views import RecommendCropView, UserHistoryView, SoilCardOCRView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/register/', CreateUserView.as_view(), name="register"),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name="obtain_token"),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name="refresh_token"),
+    path('api-auth/', include("rest_framework.urls")),
+    path('api/auth/firebase/', FirebaseAuthView.as_view(), name='firebase_auth'),
+    # Link Email/Google to Phone Account
+    path('api/auth/link/', LinkAccountView.as_view(), name='link_account'),
+    path('api/me/', UserDetailView.as_view(), name='user_detail'),
+    path('predict/', RecommendCropView.as_view(), name='predict'),
+    path('history/', UserHistoryView.as_view(), name='history'),
+    path('ocr-soil-card/', SoilCardOCRView.as_view(), name='ocr_soil_card'),
+    
+]
