2f75cdd020e9e1b7d7c6b9483ca457c470707bf4
Rohan M <rohan110620@gmail.com>
2026-03-26 20:17:46 +0530

    model changes

--- recommendation/models.py
+++ recommendation/models.py
@@ -1,13 +1,8 @@
-# recommendation/models.py
 from django.db import models
-from django.conf import settings  # To refer to the User model
+from django.conf import settings 
 
 class CropPrediction(models.Model):
-    # Link to the user who made the prediction
-    # on_delete=models.CASCADE means if user is deleted, their history is deleted
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='predictions')
-    
-    # Input Features
     nitrogen = models.FloatField()
     phosphorus = models.FloatField()
     potassium = models.FloatField()
@@ -15,11 +10,7 @@ class CropPrediction(models.Model):
     humidity = models.FloatField()
     ph = models.FloatField()
     rainfall = models.FloatField()
-    
-    # The Result
     predicted_crop = models.CharField(max_length=100)
-    
-    # Metadata
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
