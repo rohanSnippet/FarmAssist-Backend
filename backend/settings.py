867b08d43efb5dee9ed86ca2264cf4aed943d9cf
Rohan M <rohan110620@gmail.com>
2026-05-06 22:03:55 +0530

    settings chagne

--- backend/settings.py
+++ backend/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.gis',
     'django.contrib.staticfiles',
     'api',
     'recommendation',
